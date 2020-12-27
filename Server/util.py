440dac8af7ab2fe6a509e59b8e2efe4f58535b4e
uma-ram <70577635+uma-ram@users.noreply.github.com>
2020-12-27 22:00:15 +0530

    initial commit

--- Server/util.py
+++ Server/util.py
@@ -41,22 +41,6 @@ def get_loan_status(gender,married,education,self_employed,applicant_income,coap
 
 
 
-'''def get_estimated_price(location,sqft,bhk,bath):
-    try:
-        loc_index = __data_columns.index(location.lower())
-    except:
-        loc_index = -1
-
-    x = np.zeros(len(__data_columns))
-    x[0] = sqft
-    x[1] = bath
-    x[2] = bhk
-    if loc_index>=0:
-        x[loc_index] = 1
-
-    return round(__model.predict([x])[0],2)'''
-
-
 def load_saved_artifacts():
     print("loading saved artifacts...start")
     global  __data_columns
